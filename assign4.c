a16bc1f7f4f443cc81e5e067880cfa518c5afa7f
kamillamamatova <kamillamamatova.km@gmail.com>
2025-01-20 20:15:25 -0500

    Update assign4.c

--- assign4.c
+++ assign4.c
@@ -4,6 +4,13 @@
 
 #define PI 3.14159265358979323846
 
+/* This program is designed to solve a "magic containment" problem, 
+where you need to fit a given number of spherical objects 
+(referred to as "motes") into cuboidal containers 
+(referred to as "devices"). The goal is to minimize the total volume 
+of motes that cannot be contained by any device. If a mote does not 
+fit into any device, its volume contributes to the "uncontained volume." */
+
 typedef struct {
     double volume;
     int original_index;
